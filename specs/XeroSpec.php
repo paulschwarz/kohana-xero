41f56916d0121efdc8a4247a9d7b71439e1eaa27
Paul Schwarz <paulsschwarz@gmail.com>
2012-06-16 21:04:24 +0300

    Added specs directory.

---
+++ specs/XeroSpec.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once 'vendor/php-xero/xero.php';
+
+class DescribeXero extends \PHPSpec\Context {
+
+	public function itShouldDoSomething()
+	{
+        throw new Exception('not implemented');
+	}
+
+}
